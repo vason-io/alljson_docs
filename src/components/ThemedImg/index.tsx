cf6e6810c07942071591db9274270bc065fc5aa1
Maksim Pachkovskiy <9646520@gmail.com>
2025-10-16 10:58:31 +0200

    fix links to imgs

--- src/components/ThemedImg/index.tsx
+++ src/components/ThemedImg/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ThemedImage from '@docusaurus/theme-classic/lib/theme/ThemedImage';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export const ThemedImg = ({ name, alt }: { name: string; alt?: string }) => (
   <ThemedImage
     alt={alt ?? name.replace('_', ' ')}
     sources={{
-      light: `/img/${name}_light.png`,
-      dark: `/img/${name}_dark.png`,
+      light: useBaseUrl(`/img/${name}_light.png`),
+      dark: useBaseUrl(`/img/${name}_dark.png`),
     }}
   />
 );
